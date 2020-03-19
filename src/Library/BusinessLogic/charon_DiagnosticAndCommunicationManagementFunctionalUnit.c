c7ae58b9cd673eba1a2a3ee0903363451c8ffbfa
Florian Kaup <kaup.florian@sentinelsw.de>
2020-03-19 12:53:13 +0100

    first idea of DiagnosticAndCommunicationManagement

---
+++ src/Library/BusinessLogic/charon_DiagnosticAndCommunicationManagementFunctionalUnit.c
@@ -0,0 +1,50 @@
+/*
+ * uds_DiagnosticAndCommunicationManagementFunctionalUnit.c
+ *
+ *  Created on: 30.01.2020
+ *      Author: Florian Kaup
+ */
+
+
+#include "charon_DiagnosticAndCommunicationManagementFunctionalUnit.h"
+#include "charon_negativeResponse.h"
+#include "charon_types.h"
+
+typedef enum {
+    SessionType_IsoSaeReserved,
+    SessionType_defaultSession,
+    SessionType_ProgrammingSession,
+    SessionType_extendedDiagnosticSession,
+    SessionType_safetySystemDiagnosticSession
+};
+
+
+uint32_t uds_DiagnosticAndCommunicationManagementFunctionalUnit_DiagnosticSessionControl (uint8_t * receiveBuffer, uint32_t receiveBufferSize, uint8_t * transmitBuffer, uint32_t transmitBufferSize)
+{
+    struct __attribute__((packed)) {
+        uint8_t sid;
+        uint8_t diagnosticSessionType;
+    } * receivedMessage = (void*)receiveBuffer;
+
+    uint32_t transmitLength = 0;
+    if (receiveBufferSize != 2u)
+    {
+        transmitLength = uds_generateNegativeResponse(uds_responseCode_IncorrectMessageLengthOrInvalidFormat, receiveBuffer[0], transmitBuffer);
+    }
+    else
+    {
+        struct __attribute__((packed)) {
+            uint8_t sid;
+            uint8_t diagnosticSessionType;
+            uint16_t timing;
+            uint16_t enhancedTiming;
+        } * transmitMessage = (void*)transmitBuffer;
+        transmitMessage->sid = receivedMessage->sid | (uint8_t)uds_sid_PositiveResponseMask;
+        transmitMessage->diagnosticSessionType = receivedMessage->diagnosticSessionType;
+        transmitMessage->timing = __builtin_bswap16(50);
+        transmitMessage->enhancedTiming = __builtin_bswap16(500);
+        transmitLength = sizeof(*transmitMessage);
+    }
+
+    return transmitLength;
+}
---
+++ test/test_charon_DiagnosticAndCommunicationManagementFunctionalUnit.c
@@ -0,0 +1,37 @@
+/*
+ * test_uds_DiagnosticAndCommunicationManagementFunctionalUnit.c
+ *
+ *  Created on: 30.01.2020
+ *      Author: Florian Kaup
+ */
+
+
+#include <unity.h>
+#include "BusinessLogic/charon_DiagnosticAndCommunicationManagementFunctionalUnit.h"
+#include "Common/charon_negativeResponse.h"
+
+
+
+void test_uds_DiagnosticAndCommunicationManagementFunctionalUnit_DiagnosticSessionControl_sendAdditionalParameters_returnsIncorrectMessageLength (void)
+{
+    uint8_t inputData[] = {0x10, 0x01, 0xff, 0x00};
+    uint8_t outputData[100];
+    uint8_t expectedOutputData[] = {0x7f, 0x10, 0x13};
+
+    uint32_t transmitSize = uds_DiagnosticAndCommunicationManagementFunctionalUnit_DiagnosticSessionControl(inputData, sizeof(inputData), outputData, sizeof(outputData));
+
+    TEST_ASSERT_EQUAL(sizeof(expectedOutputData), transmitSize);
+    TEST_ASSERT_EQUAL_MEMORY(expectedOutputData, outputData, sizeof(expectedOutputData));
+}
+
+void test_uds_DiagnosticAndCommunicationManagementFunctionalUnit_DiagnosticSessionControl_ok_returnsTimingParameters (void)
+{
+    uint8_t inputData[] = {0x10, 0x02};
+    uint8_t outputData[100];
+    uint8_t expectedOutputData[] = {0x50, 0x02, 0x00, 0x32, 0x01, 0xF4};
+
+    uint32_t transmitSize = uds_DiagnosticAndCommunicationManagementFunctionalUnit_DiagnosticSessionControl(inputData, sizeof(inputData), outputData, sizeof(outputData));
+
+    TEST_ASSERT_EQUAL(sizeof(expectedOutputData), transmitSize);
+    TEST_ASSERT_EQUAL_MEMORY(expectedOutputData, outputData, sizeof(expectedOutputData));
+}
