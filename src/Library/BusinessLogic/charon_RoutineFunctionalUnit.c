166636f5496b6edfb57f928f9f4904e68c6d14e1
Florian Kaup <kaup.florian@sentinelsw.de>
2020-03-23 13:25:37 +0100

    test and bugfix for routine control erase flash

--- src/Library/BusinessLogic/charon_RoutineFunctionalUnit.c
+++ src/Library/BusinessLogic/charon_RoutineFunctionalUnit.c
@@ -24,10 +24,16 @@ uint32_t charon_RoutineFunctionalUnit_RoutineControl (uint8_t * receiveBuffer, u
     }
     else
     {
-        uint16_t routineIdentifier = ((uint16_t)receiveBuffer[3] << 8) | receiveBuffer[4];
-        if (routineIdentifier == 0xFF00u)
+        uint16_t routineIdentifier = ((uint16_t)receiveBuffer[2] << 8) | receiveBuffer[3];
+        if ( (routineIdentifier == 0xFF00u) && (receiveBuffer[1] == 1u) )
         {
             charon_NvmDriver_erase();
+
+            transmitBuffer[0] = receiveBuffer[0] | (uint8_t)uds_sid_PositiveResponseMask;
+            transmitBuffer[1] = receiveBuffer[1];
+            transmitBuffer[2] = receiveBuffer[2];
+            transmitBuffer[3] = receiveBuffer[3];
+            transmitLength = 4;
         }
         else
         {
--- test/test_charon_RoutineFunctionalUnit.c
+++ test/test_charon_RoutineFunctionalUnit.c
@@ -13,7 +13,19 @@
 #include "mock_charon_interface_NvmDriver.h"
 
 
-void test_charon_RoutineFunctionalUnit_RoutineControl_invalidSubfunction_returnsSubfunctionNotSupported (void)
+void test_charon_RoutineFunctionalUnit_RoutineControl_invalidSubfunctionEquals0_returnsSubfunctionNotSupported (void)
+{
+    uint8_t inputData[] = {0x31, 0x00, 0xff, 0x00};
+    uint8_t outputData[100];
+    uint8_t expectedOutputData[] = {0x7f, 0x31, 0x12};
+
+    uint32_t transmitSize = charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData), outputData, sizeof(outputData));
+
+    TEST_ASSERT_EQUAL(sizeof(expectedOutputData), transmitSize);
+    TEST_ASSERT_EQUAL_MEMORY(expectedOutputData, outputData, sizeof(expectedOutputData));
+}
+
+void test_charon_RoutineFunctionalUnit_RoutineControl_invalidSubfunctionGreater3_returnsSubfunctionNotSupported (void)
 {
     uint8_t inputData[] = {0x31, 0x04, 0xff, 0x00};
     uint8_t outputData[100];
@@ -42,6 +54,20 @@ void test_charon_RoutineFunctionalUnit_RoutineControl_invalidRoutineIdentifier_r
     uint8_t inputData[] = {0x31, 0x01, 0x00, 0x00};
     uint8_t outputData[100];
     uint8_t expectedOutputData[] = {0x7f, 0x31, 0x31};
+
+    uint32_t transmitSize = charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData), outputData, sizeof(outputData));
+
+    TEST_ASSERT_EQUAL(sizeof(expectedOutputData), transmitSize);
+    TEST_ASSERT_EQUAL_MEMORY(expectedOutputData, outputData, sizeof(expectedOutputData));
+}
+
+void test_charon_RoutineFunctionalUnit_RoutineControl_RoutineEraseFlash_returnsOk (void)
+{
+    uint8_t inputData[] = {0x31, 0x01, 0xFF, 0x00};
+    uint8_t outputData[100];
+    uint8_t expectedOutputData[] = {0x71, 0x01, 0xFF, 0x00};
+
+    charon_NvmDriver_erase_Expect();
 
     uint32_t transmitSize = charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData), outputData, sizeof(outputData));
 
