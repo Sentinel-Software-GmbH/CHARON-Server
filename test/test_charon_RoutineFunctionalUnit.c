c8bfbe919906e789018f20050c3a46a84ce95833
Florian Kaup <kaup.florian@sentinelsw.de>
2020-04-01 15:50:06 +0200

    extended unit tests for routine control

--- test/test_charon_RoutineFunctionalUnit.c
+++ test/test_charon_RoutineFunctionalUnit.c
@@ -18,7 +18,7 @@ void test_charon_RoutineFunctionalUnit_RoutineControl_invalidSubfunction_returns
     uint8_t inputData[] = {0x31, 0x04, 0xff, 0x00};
 
     charon_sendNegativeResponse_Expect(0x12, 0x31);
-    charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData));
+    TEST_ASSERT_EQUAL(uds_responseCode_SubfunctionNotSupported, charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData)));
 }
 
 void test_charon_RoutineFunctionalUnit_RoutineControl_messageTooShort_returnsIncorrectMessageLength (void)
@@ -26,7 +26,7 @@ void test_charon_RoutineFunctionalUnit_RoutineControl_messageTooShort_returnsInc
     uint8_t inputData[] = {0x31, 0x04, 0xff};
 
     charon_sendNegativeResponse_Expect(0x13, 0x31);
-    charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData));
+    TEST_ASSERT_EQUAL(uds_responseCode_IncorrectMessageLengthOrInvalidFormat, charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData)));
 }
 
 void test_charon_RoutineFunctionalUnit_RoutineControl_invalidRoutineIdentifier_returnsRequestOutOfRange (void)
@@ -34,6 +34,6 @@ void test_charon_RoutineFunctionalUnit_RoutineControl_invalidRoutineIdentifier_r
     uint8_t inputData[] = {0x31, 0x01, 0x00, 0x00};
 
     charon_sendNegativeResponse_Expect(0x31, 0x31);
-    uint32_t transmitSize = charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData));
+    TEST_ASSERT_EQUAL(uds_responseCode_RequestOutOfRange, charon_RoutineFunctionalUnit_RoutineControl(inputData, sizeof(inputData)));
 }
 
