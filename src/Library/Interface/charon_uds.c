6fcd3d565d6758917beb8a4ac2d8d148a8cb3b4c
Florian Kaup <kaup.florian@sentinelsw.de>
2020-04-07 14:17:24 +0200

    fix some minor pclint warnings

--- src/Library/Interface/charon_uds.c
+++ src/Library/Interface/charon_uds.c
@@ -6,11 +6,7 @@
  */
 
 
-#include <stdint.h>
-
 #include "charon_uds.h"
-#include "Common/charon_types.h"
-#include "HSDI/charon_interface_canisotp.h"
 #include "ComLogic/charon_SessionAndSerivceControl.h"
 
 void charon_task (void)
