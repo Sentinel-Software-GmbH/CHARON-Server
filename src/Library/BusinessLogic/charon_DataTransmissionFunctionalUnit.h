b0bb214a31ff4a04df5030d0342d6c3a071715e0
hofmann.andreas <hofmann.andreas@sentinelsw.de>
2022-03-29 16:00:22 +0200

    Corrects Authors and Headers.

--- src/Library/BusinessLogic/charon_DataTransmissionFunctionalUnit.h
+++ src/Library/BusinessLogic/charon_DataTransmissionFunctionalUnit.h
@@ -1,6 +1,6 @@
 /**
  *  Sentinel Software GmbH
- *  Copyright (C) 2022 Florian Kaup
+ *  Copyright (C) 2022 Andreas Hofmann
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
